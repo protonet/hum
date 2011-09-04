ffd00245a062be9590fbbe34563acff51bf0df8c
Philip MacIver <philip.maciver@xing.com>
2011-09-04 19:59:42 +0200

    Set no cache

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -2,6 +2,7 @@ class QueueController < ApplicationController
 
   def index
     @queue = Queue.list
+    headers['Cache-Control'] = 'no-cache'
     render :layout => false
   end
 
@@ -13,10 +14,12 @@ class QueueController < ApplicationController
 
   def next_track
     id = Queue.remove
+    headers['Cache-Control'] = 'no-cache'
     render :json => {'id' => (id ? id : '')}, :layout => false
   end
 
   def queue_has_tracks
+    headers['Cache-Control'] = 'no-cache'
     render :json => {'has_tracks' => (Queue.list.empty? ? 'false' : 'true')}, :layout => false
   end
 
